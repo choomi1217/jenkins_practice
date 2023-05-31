67a8462d96ad31890c912b8ff2c7d15a4a497249
ymcho <whdudal1217@naver.com>
2023-05-31 16:46:24 +0900

    init

--- src/main/java/cho/me/jenkins_practice/JenkinsPracticeApplication.java
+++ src/main/java/cho/me/jenkins_practice/JenkinsPracticeApplication.java
@@ -2,9 +2,15 @@ package cho.me.jenkins_practice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class JenkinsPracticeApplication {
+
+	@GetMapping("/welcome")
+	public String welcome(){
+		return "WELCOME !!";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(JenkinsPracticeApplication.class, args);
